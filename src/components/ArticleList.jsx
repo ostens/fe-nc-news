458f1ac45077723610ec7eb352f13ba33bc4111d
ostens <erinjayaustin@gmail.com>
2022-12-06 09:55:11 +0000

    remove unnecessary else

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -17,29 +17,28 @@ function ArticleList() {
   }, []);
 
   if (isLoading) return <p>Getting your articles...</p>;
-  else
-    return (
-      <ul>
-        {articles.map((article) => {
-          return (
-            <li key={article.article_id}>
-              <CardHeader
-                author={article.author}
-                created_at={article.created_at}
-                topic={article.topic}
-              />
-              <Link to={`/articles/${article.article_id}`} className="link">
-                <h2>{article.title}</h2>
-              </Link>
-              <CardFooter
-                votes={article.votes}
-                comment_count={article.comment_count}
-              />
-            </li>
-          );
-        })}
-      </ul>
-    );
+  return (
+    <ul>
+      {articles.map((article) => {
+        return (
+          <li key={article.article_id}>
+            <CardHeader
+              author={article.author}
+              created_at={article.created_at}
+              topic={article.topic}
+            />
+            <Link to={`/articles/${article.article_id}`} className="link">
+              <h2>{article.title}</h2>
+            </Link>
+            <CardFooter
+              votes={article.votes}
+              comment_count={article.comment_count}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default ArticleList;
